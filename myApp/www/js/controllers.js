b087d2cc5b27fa4c9dca67ce1328b7720ed1b796
alexv7 <alexvarjabedian7@gmail.com>
2016-07-16 20:42:10 -0400

    saving to firebase now works...needed to modify the rules to true

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats, Products) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -15,7 +15,15 @@ angular.module('starter.controllers', [])
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
-})
+
+  // this is a hack
+  $scope.image ='http://www.flightclub.com/media/catalog/product/cache/1/image/700x500/9df78eab33525d08d6e5fb8d27136e95/a/d/adidas-yeezy-boost-350-pirate-black-2016-release-pirblk-blugra-cblack-201189_1.jpg'
+
+  $scope.button = function(product){
+    console.log('button was clicked', product, $scope.image);
+    Products.saveProduct(product, $scope.image);
+  }
+})  
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
