77be82e481807f738c4a5394010fa44943c57979
alexv7 <alexvarjabedian7@gmail.com>
2016-07-23 01:13:24 -0400

    takePhoto and choosePhoto functions defined but for now console logging only

--- myApp/www/js/controllers/PhotoCtrl.js
+++ myApp/www/js/controllers/PhotoCtrl.js
@@ -2,4 +2,11 @@
 
 app.controller('PhotoCtrl', function($scope) {
  
+  $scope.takePhoto = function(){
+    console.log("take")
+  }
+  $scope.choosePhoto = function(){
+    console.log("choose")
+  }
+
 });
