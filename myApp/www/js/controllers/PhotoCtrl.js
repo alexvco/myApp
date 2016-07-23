60036a7d2baeb420f6ccbc0cf42c70bd70818cf8
alexv7 <alexvarjabedian7@gmail.com>
2016-07-23 17:06:52 -0400

    redirect user to tab-dash after they submit chat/photo, also refactored code in PhotoCtrl.js

--- myApp/www/js/controllers/PhotoCtrl.js
+++ myApp/www/js/controllers/PhotoCtrl.js
@@ -2,52 +2,36 @@
 
 app.controller('PhotoCtrl', function($scope,  $cordovaCamera, $rootScope, $state) {
  
+  var options = {
+        quality: 75,
+        destinationType: Camera.destinationType.DATA_URL,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        popoverOptions: CameraPopoverOptions,
+        targetWidth: 500,
+        targetHeight: 500,
+        saveToPhotoAlbum: false,
+  };
+
   $scope.takePhoto = function(){
     console.log("takePhoto clickec");
       // Camera Plugin
-
-      var options = {
-        quality: 75,
-        destinationType: Camera.destinationType.DATA_URL,
-        sourceType: Camera.PictureSourceType.CAMERA,
-        allowEdit: true,
-        encodingType: Camera.EncodingType.JPEG,
-        popoverOptions: CameraPopoverOptions,
-        targetWidth: 500,
-        targetHeight: 500,
-        saveToPhotoAlbum: false,
-      };
+      options.sourceType = Camera.PictureSourceType.CAMERA;
 
       $cordovaCamera.getPicture(options).then(function(photo){
         $rootScope.imgURI = "data:imagee/jpeg:base64," +photo;
         $state.go('tab.photo-detail');
       })
-
   }
-
-
 
   $scope.choosePhoto = function(){
     console.log("choosePhoto was clicked");
-
-
-    var options = {
-        quality: 75,
-        destinationType: Camera.destinationType.DATA_URL,
-        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-        allowEdit: true,
-        encodingType: Camera.EncodingType.JPEG,
-        popoverOptions: CameraPopoverOptions,
-        targetWidth: 500,
-        targetHeight: 500,
-        saveToPhotoAlbum: false,
-      };
+      options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
 
       $cordovaCamera.getPicture(options).then(function(photo){
         $rootScope.imgURI = "data:imagee/jpeg:base64," +photo;
         $state.go('tab.photo-detail');
       })
-
   }
 
 });
